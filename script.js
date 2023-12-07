b2f8cc4842eecd7ed0a3483cb6a216221f9a4abe
WAVVII <87075527+WAVVII@users.noreply.github.com>
2023-12-07 10:39:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,35 +7,36 @@ menuToggle.addEventListener('click', () => {
 })
 
 document.getElementById('animationButton').addEventListener('click', function() {
-  var animationButton = document.getElementById('animationButton');
-  var sidebar = document.getElementById('sidebar');
-  var pageHeader = document.getElementById('pageHeader');
-  var a02a = document.getElementById('a02a');
-  var a02b = document.getElementById('a02b');
+    var animationButton = document.getElementById('animationButton');
+    var sidebar = document.getElementById('sidebar');
+    var pageHeader = document.getElementById('pageHeader');
+    var a02a = document.getElementById('a02a');
+    var a02b = document.getElementById('a02b');
 
-  animationButton.style.transition = 'opacity 0.5s';
-  animationButton.style.pointerEvents = 'none';
+    animationButton.style.transition = 'opacity 0.5s';
+    animationButton.style.pointerEvents = 'none';
 
-  setTimeout(function() {
-      animationButton.style.opacity = '0';
-  }, 50);
+    setTimeout(function() {
+        animationButton.style.opacity = '0';
+    }, 50);
 
-  setTimeout(function() {
-      animationButton.style.display = '';
-      animationButton.style.opacity = '0';
-      animationButton.style.pointerEvents = 'none';
-  }, 500);
+    setTimeout(function() {
+        animationButton.style.display = '';
+        animationButton.style.opacity = '0';
+        animationButton.style.pointerEvents = 'none';
+    }, 500);
 
-  setTimeout(function() {
-      sidebar.classList.toggle('hidden');
-      pageHeader.classList.toggle('hidden');
-      
-      setTimeout(function() {
-          a02a.classList.toggle('hidden');
-      }, 200); 
-      
-      setTimeout(function() {
-          a02b.classList.toggle('hidden');
-      }, 700); 
-  }, 200); 
+    setTimeout(function() {
+        sidebar.classList.toggle('hidden');
+        pageHeader.classList.toggle('hidden');
+        // Add other section animations here
+        
+        setTimeout(function() {
+            a02b.classList.toggle('hidden');
+        }, 100); // 0.2s delay after first and second section animations
+        
+        setTimeout(function() {
+            a02a.classList.toggle('hidden');
+        }, 80); // 0.7s delay after first and second section animations
+    }, 100); // Delay for the first and second section animations
 });
